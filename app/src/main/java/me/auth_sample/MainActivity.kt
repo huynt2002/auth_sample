68a4aa921ecc26a6e5d425405510e002c0b92664
huynt2002 <uchitoyuki@gmail.com>
2024-12-14 17:31:36 +0700

    refactor

--- app/src/main/java/me/auth_sample/MainActivity.kt
+++ app/src/main/java/me/auth_sample/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import me.auth_kit_aos.auth_kit_ui.AuthView
+import me.auth_kit_aos.auth_kit_ui.components.MarkDownTest
 import me.auth_sample.ui.theme.Auth_sampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Auth_sampleTheme {
-                 AuthView()
+                AuthView()
             }
         }
     }
